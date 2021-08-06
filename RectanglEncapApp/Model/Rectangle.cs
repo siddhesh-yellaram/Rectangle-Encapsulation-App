ed8041d81e904e681a105206b09c7f3b0014155a
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 01:54:12 +0530

    Add files via upload

---
+++ RectanglEncapApp/Model/Rectangle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectanglEncapApp.Model
+{
+    class Rectangle
+    {
+        private int _height;
+        private int _width;
+        private BorderStyle _border = BorderStyle.SOLID;
+
+        public Rectangle(int width, int height)
+        {
+            _width = width;
+            _height = height;
+        }
+
+        public Rectangle(int width, int height, BorderStyle border)
+        {
+            _width = width;
+            _height = height;
+            _border = border;
+        }
+
+        public int getHeight()
+        {
+            return _height;
+        }
+
+        public int getWidth()
+        {
+            return _width;
+        }
+
+        public BorderStyle getBorder()
+        {
+            return _border;
+        }
+
+        public void setBorder(BorderStyle border)
+        {
+            _border = border;
+        }
+
+        public int calculateArea()
+        {
+            return _width * _height;
+        }
+    }
+}
---
+++ RectanglEncapApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RectanglEncapApp.Model;
+
+namespace RectanglEncapApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle(100, 120);
+            Rectangle r2 = new Rectangle(120, 200, BorderStyle.DOTTED);
+
+            Console.WriteLine(r1.getBorder());
+            Console.WriteLine(r2.getBorder());
+
+            Console.WriteLine(r1.calculateArea());
+            Console.WriteLine(r2.calculateArea());
+
+            Console.ReadLine();
+        }
+    }
+}
